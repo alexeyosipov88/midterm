bc643b678f39301469df2bb87976f64557a2544d
Sayantan Basu <sayantanbasu@live.in>
2021-06-27 21:00:42 +0000

    Skeleton

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dbParams = require('../lib/db');
 const router  = express.Router();
 /*
 users = {
@@ -11,43 +12,51 @@ users = {
 }
 */
 
-module.exports = (db) => {
-  //Looks up if an email exists or not
-const emailLooker = (emailFromDatabase, userEmail) =>{
-if(emailFromDatabase === userEmail){
-  //email exsits
- // return useridfrom database
- }
-}
-
-  router.get("/register", (req, res) => {
-    res.render();
-  });
-
-  router.post("/register", (req,res) => {
-   const userId   ;
-
-    //code
-    const errorMsg = "User email exists, please login";
-    //const userId = generateRandomString();
-    const userLogin = req.body;
-    const newUser = {
-      id,
-      email,
-      password
-    };
-
-    let user = emailLooker(userLogin.email, users);
-    //checking if user email exists or not
-    if (user) {
-      if (user.email === userLogin.email) {
-        // send error msg & early return to stop the function
-        return res.status(403).send(errorMsg);
-      }
-    }
-    users[userId] = newUser;
-    req.session["userid"] = userId;
-    res.redirect("/urls");
+module.exports = (db) =>  {
+  // const login =  function(email, password) {
+  //   return database.getUserWithEmail(email)
+  //   .then(user => {
+  //     if (bcrypt.compareSync(password, user.password)) {
+  //       return user;
+  //     }
+  //     return null;
+  //   });
+  // }
+  // exports.login = login;
+  //checking if user is already registered in our database
+const isUser = function (email, password) {
+  const vars = [email, password];
+  db.query('SELECT id FROM users WHERE name = $1 AND password = $2', vars).then((user)=>{
+    return user;
   })
-  return router;
 };
+
+  router.get('/register', (res,req)=> {
+    // res.render('register.ejs');
+        res.send('Hello');
+  })
+
+  router.post('/register', (res,req)=>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    isUser(email,password)
+    .then((user) => {
+      if(user){
+        console.log(user);
+        res.redirect('./login');
+      }
+    })
+    const vars = [name, email, password]
+    db.query('INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING', vars)
+    .then((user)=>{
+      console.log(user);
+    })
+
+
+
+  })
+
+  return router;
+}
