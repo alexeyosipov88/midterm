a80f1d5a8164c2a43df262960fc5b7abe253b385
alexeyosipov88 <aoosipov88@gmail.com>
2021-06-28 07:05:34 +0000

    create tables and inserte some rows

---
+++ db/seeds/02_animals.sql
@@ -0,0 +1,6 @@
+INSERT INTO animals (id, name)
+VALUES (1, 'Dogs'),
+VALUES (2, 'Cats'),
+VALUES (3, 'Fish'),
+VALUES (4, 'Reptiles'),
+VALUES (5, 'Small Pets');
---
+++ db/seeds/05_listings.sql
@@ -0,0 +1,21 @@
+-- Widgets table seeds here (Example)
+INSERT INTO listings (id, name, price, description, photo, created_at, user_id, animal_id, category_id)
+VALUES (1, 'Cujo Food', 3999, 'Provide your dog with a delicious meal by serving Cujo Food. Your dog will love you for this. It is also very nutritious', 'https://cdn.pixabay.com/photo/2020/05/13/21/03/dog-food-5168940_1280.jpg', 2021-06-23, 1, 1, 1)
+(2, 'Double Ceramic Cat Bowls', 499, 'Feed your cat in these beautiful two ceramic bowls. One for food, one for water', 'https://cdn.pixabay.com/photo/2018/06/14/13/03/bowls-3474823_1280.jpg', 2021-06-24, 2, 2, 2),
+(3, 'Spike Bowl Dog Squeaker', 799, 'Keep your dog active and entertained with this spiked ball.', 'https://cdn.pixabay.com/photo/2016/08/01/13/13/dog-1561011_1280.jpg', 2021-06-25, 3, 1, 3),
+(4, 'Round Bird Cage', 8999, 'This classic bird cage is ideal for canaries, finches, parakeets and other small birds. Cage comes with a wood perch.', 'https://cdn.pixabay.com/photo/2016/03/31/21/07/bird-1296205_1280.png', 2021-06-26, 4, 5, 2),
+(5, 'Giant Mealworms ', 499, 'Keep your reptile fed and happy with these giant mealworms. They are a great source of protein for bearded dragons, leopard geckos, chameleons and more.', 'https://cdn.pixabay.com/photo/2019/05/27/17/49/mealworms-4233259_1280.jpg', 2021-06-28, 1, 4, 1)
+
+
+CREATE TABLE listings {
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+  description TEXT NOT NULL,
+  is_active BOOLEAN NOT NULL DEFAULT TRUE,
+  photo VARCHAR(255),
+  created_at DATE NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON CASCADE DELETE,
+  animal_id INTEGER REFERENCES animals(id) ON CASCADE DELETE,
+  category_id INTEGER REFERENCES categories(id) ON CASCADE DELETE
+}
