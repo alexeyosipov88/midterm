f199bef5a1caaefa285ef344b263b1163096ab35
Sayantan Basu <sayantanbasu@live.in>
2021-06-29 21:57:39 +0000

    rendering pages with ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,78 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
+$(() => {
+  console.log("a lot");
+  //fetch my listings
+  $.get("/en/listing", function(data, status){
+    console.log("Data: " + data[0].name );
+    // $("#menu").append('<li><a href="#">New list item</a></li>');
+    for(list of data){
+    $(".featured").append(listing(list));
+  }
+  });
+
+  $("#search-button").click(function() {
+    const search = $("#search-input").val() ;
+    console.log("$%^&*_---",search);
+    $.post("/en/search",
+  {
+    search
+  },
+  function(data, status){
+    // alert("Data: " + data + "\nStatus: " + status);
+    console.log(data);
+    $("featured_listings_title").empty();
+    $("featured_listings_title").append(`<h3>
+    Search Results
+  </h3>`);
+
+    $(".featured").empty();
+    for(item of data){
+      $(".featured").append(searchTemplate(item));
+    }
+
+  });
+  })
+
+  // $.ajax({
+  //   method: "GET",
+  //   url: '/en/listing'
+  // }).then((listings) => {
+  //   // alert(listings);
+  //   console.log("listings are: @#$%_*& ",listings);
+  //   // for(list of listings) {
+  //   //   $("<div>").text(list).appendTo($("#listing"));
+  //   // }
+  // });
+
+});
+
+function listing(list) {
+const template = `<div class="row gx-5">
+<div class="col-6 col-sm-3">
+  <div class="circular--landscape">
+    <img class="listing-image" src="${list.photo}" />
+  </div>
+  <div class="item_description">
+    ${list.name}<BR>
+    $${list.price}
+  </div>
+</div>
+`
+
+return template ;
+}
+
+function searchTemplate(item) {
+const template = `
+<div class="row gx-5">
+<div class="col-6 col-sm-3">
+  <div class="circular--landscape">
+    <img class="listing-image" src="${list.photo}" />
+  </div>
+  <div class="item_description">
+    ${list.name}<BR>
+    $${list.price}
+  </div>
+</div>
+`
+return template ;
+}
--- routes/endpoints.js
+++ routes/endpoints.js
@@ -30,7 +30,38 @@ const database = require('./database');
 
 module.exports = (db) => {
 
+  // Home page
+    router.get('/listing', (req, res) => {
+    // console.log('@#$%^&*');
+    db.query(`SELECT * FROM listings`)
+      .then(data => {
+        console.log(data.rows);
+        const listings = data.rows;
+        //  console.log(listings);
+        res.json(listings);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
+  router.post('/search', (req,res) =>{
+    console.log(req.body);
+    db.query(`SELECT * FROM listings where name LIKE '${req.body.search}%'`)
+    .then(data => {
+      const listings = data.rows;
+      res.json(listings);
+    })
+  })
+  // router.get("/products", (req, res) => {
+  //   db.query(`SELECT * FROM listings`)
+  //   .then((data)=>{
+  //     const listings = data.rows ;
+  //     res.json({listings})
+  //   })
+  // })
 
 return router;
 }
