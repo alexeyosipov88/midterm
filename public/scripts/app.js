c51d15e6a81e3811564b4fda1dc066f919a09df6
Vivian Cheung <hi@viviancheung.ca>
2021-06-30 03:39:21 +0000

    fix loading of index.html, search function, filter button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,52 @@
+/* eslint-disable func-style */
 $(() => {
   console.log("a lot");
   //fetch my listings
-  $.get("/en/listing", function(data, status){
-    console.log("Data: " + data[0].name );
+  $.get("/en/listing", function(data, status) {
+    console.log("Data: " + data[0].name);
     // $("#menu").append('<li><a href="#">New list item</a></li>');
-    for(list of data){
-    $(".featured").append(listing(list));
-  }
+    for (list of data) {
+      $(".featured").append(listing(list));
+    }
   });
 
   $("#search-button").click(function() {
-    const search = $("#search-input").val() ;
+    const search = $("#search-input").val();
     console.log("$%^&*_---",search);
     $.post("/en/search",
-  {
-    search
-  },
-  function(data, status){
-    // alert("Data: " + data + "\nStatus: " + status);
-    console.log(data);
-    $("featured_listings_title").empty();
-    $("featured_listings_title").append(`<h3>
-    Search Results
-  </h3>`);
+      {
+        search
+      },
+      function(data, status) {
+        // alert("Data: " + data + "\nStatus: " + status);
+        console.log(data);
+        $("featured_listings_title").empty();
+        $("featured_listings_title").append(`<h3>
+        Search Results
+        </h3>`);
 
-    $(".featured").empty();
-    for(item of data){
-      $(".featured").append(searchTemplate(item));
-    }
+        $(".featured").empty();
+        for (item of data) {
+          $(".featured").append(searchTemplate(item));
+        }
 
+      });
   });
-  })
 
-  $("#filter_button").click(function() {
+  $("#filter_by_price").click(function() {
 
-    $.get("/en/listing", function(data, status){
-      console.log("Data: " , data );
+    $.get("/en/listing", function(data, status) {
+      console.log("Data: " , data);
       $(".featured").empty();
-      for(list of data){
-      $(".featured").append(listing(list));
-    }
-  })
+      for (list of data) {
+        $(".featured").append(listing(list));
+      }
+    });
   });
 
   $(".active").click(function() {
-    $.get("/en/user") ;
-  })
+    $.get("/en/user");
+  });
   // $.ajax({
   //   method: "GET",
   //   url: '/en/listing'
@@ -60,33 +61,33 @@ $(() => {
 });
 
 function listing(list) {
-const template = `<div class="row gx-5">
-<div class="col-6 col-sm-3">
-  <div class="circular--landscape">
-    <img class="listing-image" src="${list.photo}" />
+  const template = `
+  <div class="product">
+  <div class="photo">
+  <img class="listing-image" src="${list.photo}" />
   </div>
   <div class="item_description">
-    ${list.name}<BR>
-    $${list.price}
+  ${list.name}<BR>
+  $${list.price}
   </div>
-</div>
-`
-
-return template ;
+  </div>
+`;
+  return template;
 }
 
 function searchTemplate(item) {
-const template = `
-<div class="row gx-5">
-<div class="col-6 col-sm-3">
-  <div class="circular--landscape">
-    <img class="listing-image" src="${item.photo}" />
+  const template = `
+  <div class="product">
+  <div class="photo">
+  <img class="listing-image" src="${item.photo}" />
   </div>
   <div class="item_description">
-    ${item.name}<BR>
-    $${item.price}
+  ${item.name}<BR>
+  $${item.price}
   </div>
-</div>
-`
-return template ;
+  </div>
+`;
+  return template;
 }
+
+
