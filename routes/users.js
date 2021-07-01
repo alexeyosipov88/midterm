930df5c25c526ba723d1bb6aed5d030c075dfd3f
alexeyosipov88 <aoosipov88@gmail.com>
2021-07-01 20:11:23 +0000

    add create listing with user_id as cookie

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ module.exports = (db) => {
 
   router.get("/profile/mylistings", (req, res) => {
     const user_id = req.session["user_id"];
-    db.query(`SELECT * FROM listings;`).then((result) => {
+    db.query(`SELECT * FROM listings WHERE user_id = ${req.session["user_id"]};`).then((result) => {
       const user = result.rows;
       console.log('this is user', user);
       console.log(user_id);
@@ -83,8 +83,9 @@ module.exports = (db) => {
   router.post('/post', (req, res) => {
     const listing =  req.body;
     db.query(`INSERT INTO listings (name, price, description, photo, animal_id, category_id, user_id, created_at)
-    VALUES ($1, $2, $3, $4, $5, $6, 1, now())
-    RETURNING *;`, [listing.name, listing.price, listing.description, listing.photo, listing.animal_id, listing.category_id]).then((result) => {
+    VALUES ($1, $2, $3, $4, $5, $6, $7, now())
+    RETURNING *;`, [listing.name, listing.price, listing.description, listing.photo, listing.animal_id, listing.category_id, req.session["user_id"]]).then((result) => {
+      console.log(result);
       res.send(result);
     });
 
